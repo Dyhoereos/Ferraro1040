21b57e3c9dcadcabfa742849736926d59ca84d89
Dennis <dennis.chau@outlook.com>
2017-02-15 10:23:32 -0800

    Unit tests and function for validating urls for a range of dates

--- app/src/main/java/com/crisisera/ferraro1040/Downloader.java
+++ app/src/main/java/com/crisisera/ferraro1040/Downloader.java
@@ -1,25 +1,72 @@
 package com.crisisera.ferraro1040;
 
+import android.util.Log;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Calendar;
+import java.util.LinkedHashMap;
+import java.util.Locale;
 
 /**
  * Created by Arcui on 02/14/2017.
  */
 
 public class Downloader {
-    private Calendar current;
     private String urlPrefix = "http://iphone.tsn.ca/tsnpodcasts/tsnradio/vancouver/ferraro_";
 
-    public Downloader(){
-        this.current = Calendar.getInstance();
-    }
+//    public Downloader(){
+//        this.current = Calendar.getInstance();
+//    }
 
     public String getDate(int offset){
+        Calendar current = Calendar.getInstance();
         current.add(Calendar.DATE, offset);
 
-        String month = Integer.toString((current.get(Calendar.MONTH)+1));
+        String month = (current.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.CANADA)).toLowerCase();
         String day = Integer.toString(current.get(Calendar.DATE));
         String year = Integer.toString(current.get(Calendar.YEAR));
         return month+day+year;
     }
+
+    public String getURL(int offset){
+        return urlPrefix + getDate(offset) + ".mp3";
+    }
+
+    public boolean validURL(String url){
+        HttpURLConnection connection;
+        boolean result = false;
+        try{
+            connection = (HttpURLConnection) new URL(url).openConnection();
+            connection.setConnectTimeout(3000);
+            connection.setRequestMethod("HEAD");
+            result = connection.getResponseCode() == HttpURLConnection.HTTP_OK;
+            System.out.println("response code for " + url + " " + connection.getResponseCode());
+//            Log.d("response code: ", Integer.toString(connection.getResponseCode()));
+            connection.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e("connection error", e.toString());
+        }
+        return result;
+    }
+
+    public LinkedHashMap<String, Boolean> getRangeOfStatus(){
+        return getRangeOfStatus(-1);
+    }
+
+    public LinkedHashMap<String, Boolean> getRangeOfStatus(int daysFromToday){
+        int offset = daysFromToday * -1 + 1;
+        LinkedHashMap<String, Boolean> map = new LinkedHashMap<>();
+        for (int i = offset; i <= 0; i++){
+            String date        = getDate(i);
+            String url         = getURL(i);
+            boolean fileExists = validURL(url);
+
+            map.put(date, fileExists);
+        }
+        return map;
+    }
+
+
 }
--- app/src/test/java/com/crisisera/ferraro1040/URLCheckAndDownload.java
+++ app/src/test/java/com/crisisera/ferraro1040/URLCheckAndDownload.java
@@ -4,60 +4,93 @@ package com.crisisera.ferraro1040;
  * Created by Arcui on 02/14/2017.
  */
 
-import android.util.Log;
-
 import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
-import java.security.InvalidParameterException;
-import java.util.Calendar;
+import java.util.LinkedHashMap;
 
 import static org.junit.Assert.*;
-import static org.junit.rules.ExpectedException.*;
 
 public class URLCheckAndDownload {
     Downloader downloader;
-
-//    @Rule
-//    public final ExpectedException exception = none();
 
     @Before
     public void initDownloader(){
         this.downloader = new Downloader();
     }
 
-//    /* Testing Calendar object */
+    /* Testing Calendar object */
 
     @Test
     public void validateCurrentDateEqual() {
-        String month = "2";
-        String day = "14";
-        String year = "2017";
+        String month    = "2";
+        String day      = "14";
+        String year     = "2017";
         String expected = month+day+year;
-        String actual = downloader.getDate(0);
+        String actual   = downloader.getDate(0);
         assertEquals(expected, actual);
     }
 
     @Test
     public void validateCurrentDateDifferent() {
-        String month = "2";
-        String day = "14";
-        String year = "2017";
+        String month    = "2";
+        String day      = "14";
+        String year     = "2017";
         String expected = month+day+year;
-        String actual = downloader.getDate(-1);
+        String actual   = downloader.getDate(-1);
         assertNotEquals(expected, actual);
     }
 
     @Test
     public void validate20DaysAgo() {
-        String month = "1";
-        String day = "22";
-        String year = "2017";
+        String month    = "1";
+        String day      = "22";
+        String year     = "2017";
         String expected = month + day + year;
-        String actual = downloader.getDate(-20);
+        String actual   = downloader.getDate(-20);
         assertNotEquals(expected, actual);
+    }
+
+    /* Testing URL string */
+
+    @Test
+    public void validateTodaysURLString() {
+        String expected = "http://iphone.tsn.ca/tsnpodcasts/tsnradio/vancouver/ferraro_feb142017.mp3";
+        String actual   = downloader.getURL(0);
+        assertEquals(expected, actual);
+    }
+
+    /* Test downloading header with URL */
+
+    @Test
+    public void getValidHeader(){
+        String url             = downloader.getURL(0);
+        boolean connectionMade = downloader.validURL(url);
+        assertTrue(connectionMade);
+    }
+
+    @Test
+    public void getInvalidHeader(){
+        String url             = downloader.getURL(1);
+        boolean connectionMade = downloader.validURL(url);
+        assertFalse(connectionMade);
+    }
+
+    /* Check header for range of dates */
+
+    @Test
+    public void checkHeadersForOneWeek(){
+        LinkedHashMap<String, Boolean> expected = new LinkedHashMap<>();
+        expected.put("feb92017", true);
+        expected.put("feb102017", true);
+        expected.put("feb112017", false);
+        expected.put("feb122017", false);
+        expected.put("feb132017", true);
+        expected.put("feb142017", true);
+        expected.put("feb152017", false);
+
+        LinkedHashMap<String, Boolean> actual = downloader.getRangeOfStatus(7);
+
+        assertEquals(expected, actual);
     }
 }
