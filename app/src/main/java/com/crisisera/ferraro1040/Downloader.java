1d85e3709e0803c70970634ea5cf6644050089e1
Dennis <dennis.chau@outlook.com>
2017-02-14 18:29:45 -0800

    Updated gitignore. Added unit tests and functions for generating date strings.

--- app/src/main/java/com/crisisera/ferraro1040/Downloader.java
+++ app/src/main/java/com/crisisera/ferraro1040/Downloader.java
@@ -1,12 +1,25 @@
 package com.crisisera.ferraro1040;
 
+import java.util.Calendar;
+
 /**
  * Created by Arcui on 02/14/2017.
  */
 
 public class Downloader {
+    private Calendar current;
+    private String urlPrefix = "http://iphone.tsn.ca/tsnpodcasts/tsnradio/vancouver/ferraro_";
 
-    public static boolean returnFalse(){
-        return false;
+    public Downloader(){
+        this.current = Calendar.getInstance();
+    }
+
+    public String getDate(int offset){
+        current.add(Calendar.DATE, offset);
+
+        String month = Integer.toString((current.get(Calendar.MONTH)+1));
+        String day = Integer.toString(current.get(Calendar.DATE));
+        String year = Integer.toString(current.get(Calendar.YEAR));
+        return month+day+year;
     }
 }
--- app/src/test/java/com/crisisera/ferraro1040/URLCheckAndDownload.java
+++ app/src/test/java/com/crisisera/ferraro1040/URLCheckAndDownload.java
@@ -4,13 +4,60 @@ package com.crisisera.ferraro1040;
  * Created by Arcui on 02/14/2017.
  */
 
+import android.util.Log;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.security.InvalidParameterException;
+import java.util.Calendar;
 
 import static org.junit.Assert.*;
+import static org.junit.rules.ExpectedException.*;
 
 public class URLCheckAndDownload {
+    Downloader downloader;
+
+//    @Rule
+//    public final ExpectedException exception = none();
+
+    @Before
+    public void initDownloader(){
+        this.downloader = new Downloader();
+    }
+
+//    /* Testing Calendar object */
+
     @Test
-    public void validateURL() throws Exception {
-        assertTrue(Downloader.returnFalse());
+    public void validateCurrentDateEqual() {
+        String month = "2";
+        String day = "14";
+        String year = "2017";
+        String expected = month+day+year;
+        String actual = downloader.getDate(0);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void validateCurrentDateDifferent() {
+        String month = "2";
+        String day = "14";
+        String year = "2017";
+        String expected = month+day+year;
+        String actual = downloader.getDate(-1);
+        assertNotEquals(expected, actual);
+    }
+
+    @Test
+    public void validate20DaysAgo() {
+        String month = "1";
+        String day = "22";
+        String year = "2017";
+        String expected = month + day + year;
+        String actual = downloader.getDate(-20);
+        assertNotEquals(expected, actual);
     }
 }
